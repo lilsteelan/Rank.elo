d890fcebebd0944d2e13bf72fc2ce981411eba48
shmunkle <pilebleue2006@gmail.com>
2024-12-15 23:06:22 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "page is functional"
+
+
+@app.route('/add')
+def add():
+    return render_template("createobject.html")
